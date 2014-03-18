7638e416bac8789918ef573bc7313136fa76d91d
PREETHZ <cheriyathpree@gmail.com>
2014-03-18 20:14:32 +0530

    Delete run.sh

--- EigenMethod/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python eigenface.py  /home/hduser/MAIN\ PROJECT/CODE/testdata/ /home/hduser/MAIN\ PROJECT/CODE/final/out/ ;
-pyssim out/mean.png  "eigenmean/*" | tee /home/hduser/score.txt;
-python ttt.py ;
-./rr.sh ;
