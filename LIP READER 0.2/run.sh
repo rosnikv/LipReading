0da2cfc254373dd5679f7b9aa4b7accba91b5231
DEEPTHI <deepthi@deepthi-HP-ProBook-4540s.(none)>
2014-04-14 21:43:28 +0530

    modified using enhanced eigen method

---
+++ LIP READER 0.2/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python avg.py  /media/deepthi/01CE146C58D7E350/home/faces-master/pyfaces/PyFaces/testdata/ /media/deepthi/01CE146C58D7E350/home/faces-master/pyfaces/PyFaces/out/ ;
+python pyfaces.py -i /media/deepthi/01CE146C58D7E350/home/faces-master/pyfaces/PyFaces/out/mean.png -d /media/deepthi/01CE146C58D7E350/home/faces-master/pyfaces/PyFaces/eigenmean;
